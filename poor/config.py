e0e911a23c5753da5e1471e5f39d86f5a9753ed3
Osmo Salomaa <otsaloma@iki.fi>
2014-04-19 04:36:14 +0300

    Allow nesting.

--- poor/config.py
+++ poor/config.py
@@ -60,9 +60,35 @@ class ConfigurationStore(AttrDict):
             return [self._coerce(x, ref[0]) for x in value]
         return type(ref)(value)
 
-    def get_default(self, name):
-        """Get the default value of option."""
-        return copy.deepcopy(DEFAULTS[name])
+    def _comment_unmodified(self, root=None, defaults=None):
+        """Return values with those at default commented out."""
+        root = (self if root is None else root)
+        defaults = (DEFAULTS if defaults is None else defaults)
+        out = {}
+        for name, value in root.items():
+            if isinstance(value, dict):
+                value = self._comment_unmodified(value,
+                                                 defaults.setdefault(name, {}))
+
+            else:
+                if name in defaults:
+                    if value == defaults[name]:
+                        name = "# {}".format(name)
+            out[name] = copy.deepcopy(value)
+        return out
+
+    def get_default(self, option):
+        """
+        Get the default value of `option`.
+
+        For nested keys, `option` can be a dotted string,
+        e.g. 'router.mycoolrouter.type'.
+        """
+        defaults = DEFAULTS
+        for section in option.split(".")[:-1]:
+            defaults = defaults[section]
+        name = option.split(".")[-1]
+        return copy.deepcopy(defaults[name])
 
     def read(self, path=None):
         """Read values of options from JSON file at `path`."""
@@ -71,26 +97,75 @@ class ConfigurationStore(AttrDict):
         if not os.path.isfile(path): return
         try:
             with open(path, "r", encoding="utf_8") as f:
-                values = json.load(f)
+                self._update(json.load(f))
         except Exception as error:
             return print("Failed to read file {}: {}"
                          .format(repr(path), str(error)),
                          file=sys.stderr)
 
+    def _register(self, values, root=None, defaults=None):
+        """Add entries for `values` if missing."""
+        root = (self if root is None else root)
+        defaults = (DEFAULTS if defaults is None else defaults)
+        for name, value in values.items():
+            if isinstance(value, dict):
+                self._register(values[name],
+                               root.setdefault(name, AttrDict()),
+                               defaults.setdefault(name, {}))
+
+            else:
+                root.setdefault(name, copy.deepcopy(value))
+                defaults.setdefault(name, copy.deepcopy(value))
+
+    def register_router(self, name, values):
+        """
+        Add configuration `values` for router `name` if missing.
+
+        e.g. calling ``register_router("foo", {"type": "car"})`` will make type
+        available as ``poor.conf.router.foo.type``.
+        """
+        self._register({"router": {name: values}}, self)
+
+    def set(self, option, value):
+        """
+        Set the value of `option`.
+
+        For nested keys, `option` can be a dotted string,
+        e.g. 'router.mycoolrouter.type'.
+        """
+        root = self
+        for section in option.split(".")[:-1]:
+            if not section in root:
+                # Create missing hierarchies.
+                root[section] = AttrDict()
+            root = root[section]
+        name = option.split(".")[-1]
+        root[name] = copy.deepcopy(value)
+
+    def _update(self, values, root=None, defaults=None, path=()):
+        """Load values of options after validation."""
+        root = (self if root is None else root)
+        defaults = (DEFAULTS if defaults is None else defaults)
         for name, value in values.items():
             # Ignore options commented out.
             if name.startswith("#"): continue
-            try:
-                # Be liberal, but careful in what to accept.
-                self[name] = self._coerce(value, DEFAULTS[name])
-            except Exception as error:
-                print("Discarding bad option-value pair ({}, {}): {}"
-                      .format(repr(name), repr(value), str(error)),
-                      file=sys.stderr)
+            if isinstance(value, dict):
+                self._update(value,
+                             root.setdefault(name, AttrDict()),
+                             defaults.setdefault(name, {}),
+                             (path + (name,)))
 
-    def set(self, option, value):
-        """Set the value of `option`."""
-        self[option] = copy.deepcopy(value)
+            else:
+                try:
+                    if name in defaults:
+                        # Be liberal, but careful in what to accept.
+                        value = self._coerce(value, defaults[name])
+                    root[name] = copy.deepcopy(value)
+                except Exception as error:
+                    full_name = ".".join(path + (name,))
+                    print("Discarding bad option-value pair ({}, {}): {}"
+                          .format(repr(full_name), repr(value), str(error)),
+                          file=sys.stderr)
 
     def write(self, path=None):
         """Write values of options to JSON file at `path`."""
@@ -99,12 +174,7 @@ class ConfigurationStore(AttrDict):
         directory = os.path.dirname(path)
         directory = poor.util.makedirs(directory)
         if directory is None: return
-        out = {}
-        for name, value in self.items():
-            if value == DEFAULTS[name]:
-                # Comment out values still at default.
-                name = "# {}".format(name)
-            out[name] = value
+        out = self._comment_unmodified()
         try:
             with open(path, "w", encoding="utf_8") as f:
                 json.dump(out, f, ensure_ascii=False, indent=4, sort_keys=True)
