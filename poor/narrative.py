2cfd3daa7ea5b379a128bb089710dfab59b59a4f
Osmo Salomaa <otsaloma@iki.fi>
2014-07-14 04:01:54 +0300

    Fix spelling.

--- poor/narrative.py
+++ poor/narrative.py
@@ -268,7 +268,7 @@ class Narrative:
         for i in list(reversed(range(len(x)-1))):
             dist = poor.util.calculate_distance(x[i], y[i], x[i+1], y[i+1])
             if dist < 0.001:
-                # Consequtive duplicate points will cause problems for
+                # Consecutive duplicate points will cause problems for
                 # calculations that determine when to show narration related
                 # to a maneuver point. We need to drop these.
                 self.x.pop(i)
