05f008c1a693e5c8e7fafe86f51ad40ddbfddd1f
Osmo Salomaa <otsaloma@iki.fi>
2016-10-28 00:55:10 +0300

    Use Python 3 string formatting, remove unused fields

--- routers/osmscout.py
+++ routers/osmscout.py
@@ -16,56 +16,53 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Routing using Mapzen Turn-by-Turn.
+Routing using OSM Scout Server.
 
 https://github.com/rinigus/osmscout-server
 """
 
 import copy
-import json
 import poor
 import urllib.parse
 
 CONF_DEFAULTS = {"type": "car"}
 
-ICONS = { "flag": "flag",
-          "start": "depart",
-          "destination": "arrive",
-          "straight": "continue",
-          "turn-slight-right": "turn-slight-right",
-          "turn-right": "turn-right",
-          "turn-sharp-right": "turn-sharp-right",
-          "turn-sharp-left": "turn-sharp-left",
-          "turn-left": "turn-left",
-          "turn-slight-left": "turn-slight-left",
-          "merge": "merge-slight-left",
-          "roundabout-enter": "roundabout",
-          "roundabout-exit": "off-ramp-slight-right",
-          "motorway-change": "flag",
-          "motorway-leave": "off-ramp-slight-right",
-          "none": "flag"
+ICONS = {
+    "destination": "arrive",
+    "flag": "flag",
+    "merge": "merge-slight-left",
+    "motorway-change": "flag",
+    "motorway-leave": "off-ramp-slight-right",
+    "none": "flag",
+    "roundabout-enter": "roundabout",
+    "roundabout-exit": "off-ramp-slight-right",
+    "start": "depart",
+    "straight": "continue",
+    "turn-left": "turn-left",
+    "turn-right": "turn-right",
+    "turn-sharp-left": "turn-sharp-left",
+    "turn-sharp-right": "turn-sharp-right",
+    "turn-slight-left": "turn-slight-left",
+    "turn-slight-right": "turn-slight-right",
 }
 
-URL = ("http://localhost:8553/v1/route?"
-       "type={type}"
-       "&radius={radius}")
-       
+URL = "http://localhost:8553/v1/route?type={type}"
 cache = {}
+
+def render_endpoint(i, point):
+    """Return URL component for given endpoint."""
+    if isinstance(point, (list, tuple)):
+        return ("&p[{i:d}][lng]={x:.6f}&p[{i:d}][lat]={y:.6f}"
+                .format(i=i, x=point[0], y=point[1]))
+    point = urllib.parse.quote_plus(point)
+    return "&p[{i:d}][search]={point}".format(i=i, point=point)
 
 def route(fm, to, params):
     """Find route and return its properties as a dictionary."""
     type = poor.conf.routers.osmscout.type
-    radius = 1000.0
-
     url = URL.format(**locals())
-    for i, p in enumerate([fm, to]):
-        if isinstance(p, (list, tuple)):
-            x, y = p[0], p[1]
-            url += "&p[%d][lng]=%0.8f" % (i,x)
-            url += "&p[%d][lat]=%0.8f" % (i,y)
-        else:
-            url += ("&p[%d][search]=" % i) + urllib.parse.quote_plus(p)
-            
+    for i, point in enumerate((fm, to)):
+        url += render_endpoint(i, point)
     with poor.util.silent(KeyError):
         return copy.deepcopy(cache[url])
     result = poor.http.request_json(url)
@@ -77,11 +74,8 @@ def route(fm, to, params):
         narrative=maneuver["instruction"],
         duration=float(maneuver["time"]),
         length=float(maneuver["length"]),
-        post=maneuver.get("verbal_post_transition_instruction", None),
-        pre=maneuver.get("verbal_pre_transition_instruction", None) 
     ) for maneuver in result["maneuvers"]]
     route = dict(x=x, y=y, maneuvers=maneuvers)
     if route and route["x"]:
         cache[url] = copy.deepcopy(route)
-
     return route
