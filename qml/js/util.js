40b6105760b5ed7527b40d2daaafe858d17ec9f2
Osmo Salomaa <otsaloma@iki.fi>
2018-01-19 01:37:24 +0200

    Remove obsolete functions

--- qml/js/util.js
+++ qml/js/util.js
@@ -16,15 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Calculations below between longitude/latitude coordinates,
- * Mercator-projected coordinates and pixel positions on screen
- * assume a spherical Mercator map canvas.
- *
- * http://en.wikipedia.org/wiki/Mercator_projection
- * http://wiki.openstreetmap.org/wiki/Mercator
- */
-
 function addProperties(items, name, value) {
     // Assign name to value in-place in all given items.
     for (var i = 0; i < items.length; i++)
@@ -35,18 +26,6 @@ function appendAll(model, items) {
     // Append all items to model.
     for (var i = 0; i < items.length; i++)
         model.append(items[i]);
-}
-
-function deg2rad(deg) {
-    // Convert degrees to radians.
-    return deg / 180 * Math.PI;
-}
-
-function eucd(x1, y1, x2, y2) {
-    // Calculate euclidean distance.
-    var xd = x2 - x1;
-    var yd = y2 - y1;
-    return Math.sqrt(xd*xd + yd*yd);
 }
 
 function findMatches(query, candidates, max) {
@@ -102,25 +81,12 @@ function median(x) {
     if (x.length === 1) return x[0];
     x = x.slice();
     x.sort(function(a, b) {
-        return a-b;
+        return a - b;
     });
     var i = Math.floor(x.length / 2);
     if (x.length % 2 === 1)
         return x[i];
     return (x[i-1] + x[i]) / 2;
-}
-
-function rad2deg(rad) {
-    // Convert radians to degrees.
-    return rad / Math.PI * 180;
-}
-
-function removeMapItems(map, items) {
-    // Remove map items from map and destroy them.
-    for (var i = 0; i < items.length; i++) {
-        map.removeMapItem(items[i]);
-        items[i].destroy();
-    }
 }
 
 function siground(x, n) {
@@ -135,38 +101,4 @@ function sortDefaultFirst(providers) {
         providers[i]["default"] &&
             providers.splice(0, 0, providers.splice(i, 1)[0]);
     }
-}
-
-function xcoord2xpos(x, xmin, xmax, width) {
-    // Convert X-coordinate to pixel X-position on screen.
-    return Math.round((x - xmin) * (width / (xmax - xmin)));
-}
-
-function xpos2xcoord(x, xmin, xmax, width) {
-    // Convert screen pixel X-position to X-coordinate.
-    return xmin + (x / (width / (xmax - xmin)));
-}
-
-function ycoord2ymercator(y) {
-    // Convert Y-coordinate to Mercator projected Y-coordinate.
-    return Math.log(Math.tan(Math.PI/4 + deg2rad(y)/2));
-}
-
-function ycoord2ypos(y, ymin, ymax, height) {
-    // Convert Y-coordinate to pixel Y-position on screen.
-    ymin = ycoord2ymercator(ymin);
-    ymax = ycoord2ymercator(ymax);
-    return Math.round((ymax - ycoord2ymercator(y)) * (height / (ymax - ymin)));
-}
-
-function ymercator2ycoord(y) {
-    // Convert Mercator projected Y-coordinate to Y-coordinate.
-    return rad2deg(2 * Math.atan(Math.exp(y)) - Math.PI/2);
-}
-
-function ypos2ycoord(y, ymin, ymax, height) {
-    // Convert screen pixel Y-position to Y-coordinate.
-    ymin = ymercator2ycoord(ymin);
-    ymax = ymercator2ycoord(ymax);
-    return (ymin + (ymercator2ycoord(y) / (height / (ymax - ymin))));
 }
