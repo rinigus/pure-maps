c72ccc2e31a8526338e0ee67a9558f3ae85ff02b
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2022-01-29 10:03:01 +0100

    Pin click dependency versions

--- packaging/click/prepare-deps.sh
+++ packaging/click/prepare-deps.sh
@@ -5,12 +5,13 @@ ENABLE_PICOTTS=${ENABLE_PICOTTS:-1}
 
 set -Eeuo pipefail
 
-# Setup paths where clickable will look for the sources
 ROOT_DIR=$(git rev-parse --show-toplevel)
+CLONE_ARGS="--recursive --shallow-submodules --depth 1"
 MAPBOX_GL_NATIVE_SRC_DIR=$ROOT_DIR/libs/mapbox-gl-native
 MAPBOX_GL_QML_SRC_DIR=$ROOT_DIR/libs/mapbox-gl-qml
 QMLRUNNER_SRC_DIR=$ROOT_DIR/libs/qmlrunner
 MIMIC_SRC_DIR=$ROOT_DIR/libs/mimic
+MIMIC_VERSION="1.3.0.1"
 PICOTTS_SRC_DIR=$ROOT_DIR/libs/picotts
 S2GEOMETRY_SRC_DIR=$ROOT_DIR/libs/s2geometry
 
@@ -18,15 +19,15 @@ S2GEOMETRY_SRC_DIR=$ROOT_DIR/libs/s2geometry
 rm -rf $MAPBOX_GL_NATIVE_SRC_DIR $MAPBOX_GL_QML_SRC_DIR $QMLRUNNER_SRC_DIR $MIMIC_SRC_DIR $PICOTTS_SRC_DIR $S2GEOMETRY_SRC_DIR
 
 # Download sources
-git clone -b mapbox-update-200607 https://github.com/rinigus/mapbox-gl-native.git $MAPBOX_GL_NATIVE_SRC_DIR --recurse-submodules  --shallow-submodules
-git clone -b 1.7.5 https://github.com/rinigus/mapbox-gl-qml.git $MAPBOX_GL_QML_SRC_DIR
-git clone https://github.com/rinigus/qmlrunner.git $QMLRUNNER_SRC_DIR
-git clone https://github.com/rinigus/s2geometry.git $S2GEOMETRY_SRC_DIR
+git clone -b mapbox-update-200607 ${CLONE_ARGS} https://github.com/rinigus/mapbox-gl-native.git $MAPBOX_GL_NATIVE_SRC_DIR
+git clone -b 1.7.5 ${CLONE_ARGS} https://github.com/rinigus/mapbox-gl-qml.git $MAPBOX_GL_QML_SRC_DIR
+git clone -b 1.0.2 ${CLONE_ARGS} https://github.com/rinigus/qmlrunner.git $QMLRUNNER_SRC_DIR
+git clone -b 0.9.0+git2 ${CLONE_ARGS} https://github.com/rinigus/s2geometry.git $S2GEOMETRY_SRC_DIR
 
 if [ "$ENABLE_MIMIC" == "1" ] ; then
-	wget -qO- https://github.com/MycroftAI/mimic1/archive/1.2.0.2.tar.gz  | tar -xzv && mv mimic1-1.2.0.2 $MIMIC_SRC_DIR
+	wget -qO- https://github.com/MycroftAI/mimic1/archive/${MIMIC_VERSION}.tar.gz  | tar -xzv && mv mimic1-${MIMIC_VERSION} $MIMIC_SRC_DIR
 fi
 
 if [ "$ENABLE_PICOTTS" == "1" ] ; then
-	git clone https://github.com/jonnius/pkg-picotts.git $PICOTTS_SRC_DIR
+	git clone ${CLONE_ARGS} https://github.com/jonnius/pkg-picotts.git $PICOTTS_SRC_DIR
 fi
