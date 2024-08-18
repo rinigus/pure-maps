a13591d5e95881fc9f67491a9753a4dea6b98a77
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2024-08-18 18:26:22 +0200

    Switch s2geometry to upstream v0.11.1 for click build

--- packaging/click/prepare-deps.sh
+++ packaging/click/prepare-deps.sh
@@ -13,16 +13,18 @@ QMLRUNNER_SRC_DIR=$ROOT_DIR/libs/qmlrunner
 MIMIC_SRC_DIR=$ROOT_DIR/libs/mimic
 MIMIC_VERSION="1.3.0.1"
 PICOTTS_SRC_DIR=$ROOT_DIR/libs/picotts
+ABSEIL_SRC_DIR=$ROOT_DIR/libs/abseil
 S2GEOMETRY_SRC_DIR=$ROOT_DIR/libs/s2geometry
 
 # Remove old downloads
-rm -rf $MAPLIBRE_GL_NATIVE_SRC_DIR $MAPBOX_GL_QML_SRC_DIR $QMLRUNNER_SRC_DIR $MIMIC_SRC_DIR $PICOTTS_SRC_DIR $S2GEOMETRY_SRC_DIR
+rm -rf $MAPLIBRE_GL_NATIVE_SRC_DIR $MAPBOX_GL_QML_SRC_DIR $QMLRUNNER_SRC_DIR $MIMIC_SRC_DIR $PICOTTS_SRC_DIR $S2GEOMETRY_SRC_DIR $ABSEIL_SRC_DIR
 
 # Download sources
 git clone -b qt-v2.0.1 ${CLONE_ARGS} https://github.com/maplibre/maplibre-gl-native.git $MAPLIBRE_GL_NATIVE_SRC_DIR
 git clone -b 2.1.1 ${CLONE_ARGS} https://github.com/rinigus/mapbox-gl-qml.git $MAPBOX_GL_QML_SRC_DIR
 git clone -b 1.0.2 ${CLONE_ARGS} https://github.com/rinigus/qmlrunner.git $QMLRUNNER_SRC_DIR
-git clone -b 0.9.0+git2 ${CLONE_ARGS} https://github.com/rinigus/s2geometry.git $S2GEOMETRY_SRC_DIR
+git clone -b v0.11.1 ${CLONE_ARGS} https://github.com/google/s2geometry.git $S2GEOMETRY_SRC_DIR
+git clone -b 20240722.0 ${CLONE_ARGS} https://github.com/abseil/abseil-cpp.git $ABSEIL_SRC_DIR
 
 if [ "$ENABLE_MIMIC" == "1" ] ; then
 	wget -qO- https://github.com/MycroftAI/mimic1/archive/${MIMIC_VERSION}.tar.gz  | tar -xzv && mv mimic1-${MIMIC_VERSION} $MIMIC_SRC_DIR
@@ -35,3 +37,6 @@ fi
 # Apply patches
 cd $MAPLIBRE_GL_NATIVE_SRC_DIR
 git apply $ROOT_DIR/packaging/click/patches/maplibre-gl-native/*.patch
+
+cd $ABSEIL_SRC_DIR
+git apply $ROOT_DIR/packaging/click/patches/abseil/*.patch
